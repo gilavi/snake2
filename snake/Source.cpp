9efab96eb21dd66ac97e8223a4b76178715e6645
gilavi <gilavi2000@gmail.com>
2013-11-29 22:41:14 +0400

    secondc

--- snake/Source.cpp
+++ snake/Source.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 char map[10][20] = {
 	"###################",
-	"#                 #",
-	"#                 #",
-	"#                 #",
-	"#                 #",
-	"#                 #",
-	"#                 #",
 	"#@                #",
+	"#                 #",
+	"#                 #",
+	"#                 #",
+	"#                 #",
+	"#                 #",
+	"#                 #",
 	"###################",
 };
+int x = 1;
+int y = 1;
 bool game_running = true;
 
 int main(){
@@ -21,9 +23,40 @@ int main(){
 			cout << map[display] << endl;
 		}                       //damtavrda fori }
 		system("pause>nul");    //ro ar icimcimos
-		if ()
+		if (GetAsyncKeyState(VK_DOWN)){      //if DOWN dasawxisi
+			int y2 = y + 1;
+			if (map[y2][x] == ' '){              //ifi s=amowmebs masivshia tu ara @
+				map[y][x] = ' ';         /*  spacesa da @ adgilta gacvla dabla*/
+				y++;
+				map[y][x] = '@';
+			}                             //ifis dasasruli
+		}                      //damtavrda if DOWN
+		if (GetAsyncKeyState(VK_UP)){
+			int y2 = y - 1;
+			if (map[y2][x] == ' '){
+				map[y][x] = ' ';
+				y--;
+				map[y][x] = '@';
+			}                           //damtavrda while
+		}
+		if (GetAsyncKeyState(VK_RIGHT)){
+			int x2 = x + 1;
+			if (map[y][x2] == ' '){
+				map[y][x] = ' ';
+				x++;
+				map[y][x] = '@';
+			}
 
-	}                           //damtavrda while }
-	
+		}
+		if (GetAsyncKeyState(VK_LEFT)){
+			int x2 = x - 1;
+			if (map[y][x2] == ' '){
+				map[y][x] = ' ';
+				x--;
+				map[y][x] = '@';
+			}
+			
+		}
+	}
 	return 0;
 }
